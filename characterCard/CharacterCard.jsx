b710b39c98f87114225cfd356e3e94678416f073
dlandivar <d_landivar@hotmail.com>
2025-07-11 15:49:24 -0500

    routers

--- characterCard/CharacterCard.jsx
+++ characterCard/CharacterCard.jsx
@@ -39,6 +39,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
+        alignItems: 'center',
     },
     /*container: {
       flex: 1,
